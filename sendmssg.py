49a44d566e8081dd1f18214ce7787e38809160fe
royayushi <ayushiroy550@gmail.com>
2022-12-29 21:50:56 +0530

    Sent a message with Twilio.

--- sendmssg.py
+++ sendmssg.py
@@ -1,6 +1,8 @@
 import os
 from twilio.rest import Client
+from dotenv import load_dotenv
 
+load_dotenv()
 
 account_sid=os.environ["TWILIO_ACCOUNT_SID"]
 auth_token=os.environ["TWILIO_AUTH_TOKEN"]
